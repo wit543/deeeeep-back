158ccf1a0a4a84310f7c68071c2a51db36864d7c
Norawit Urailertprasert <norawit543@hotmail.com>
2017-11-26 03:04:44 +0700

    change port

--- server.py
+++ server.py
@@ -42,4 +42,4 @@ def get_prediction():
   return jsonify(predict(float(request.args.get('lat')),float(request.args.get('long'))))
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=80)
+  app.run(debug=True, host='0.0.0.0', port=5050)
